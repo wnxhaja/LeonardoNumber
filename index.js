498776975de526e2dd4658ba26304ebf1ae43d7a
Jalil Laguindab <wnxhaja@gmail.com>
2016-02-08 04:47:07 +0800

    Edit answers

--- index.js
+++ index.js
@@ -5,31 +5,28 @@
  */
 
 
-(function(){
-
-    var leonardo = require('./lib/leonardo');
-    var fibonacci = require('./lib/fibonacci');
-
-})();
+module.exports.leonardo = require('./lib/leonardo');
 
 /**
- *  1.  ./lib/leonardo.js , O(n)
+ *  1.  ./lib/leonardo.js, function name: leonardo, O(n)
  *
  *  2.  L(n) = 2*F(n+1)-1
  *
- *  3.  O(n) ,  What I know is that fibonacci can be optimized to O(log n) using the matrix identity and memoization.
+ *  3.  O(n) ,  function name : fibLeonardo
+ *              What I know is that fibonacci can be optimized to O(log n) using the matrix identity and memoization.
  *              Therefore, we can use the formula in problem 2 to calculate the Leonardo then it optimized the time
  *              complexity to O(log n).
  *
  *              With my current implementation , I can apply memoization to it then as the data set goes larger then
  *              the time complexity will move closer to O(log n).
  *
- *  4. ./lib/leonardo.js
+ *  4. ./lib/leonardo.js, function name: forwardLeonardo, backwardLeonardo, forwardBackwardLeonardo
  *
- *  5. [67,-43,25,-17,9,-7,3,-3,1,-1]
+ *  5. [-10..-1] = [67,-43,25,-17,9,-7,3,-3,1,-1]
  *
- *  6. ./lib/leonardo.js
+ *  6. ./lib/leonardo.js, function name: forwardLeonardo, backwardLeonardo
  *
+ *  7-10 No Answer
  *
  */
 
